c04fd603672e6924b7caae558431ba91757908a5
Bayel Chief <marvel.vovo@gmail.com>
2023-04-18 22:43:10 +0600

    add CSS modules

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import headCss from './Header.module.css';
 
 const Header = () => {
-    return  <header className='header'>
+    return  <header className={headCss.header}>
     <img src='https://w7.pngwing.com/pngs/187/532/png-transparent-logo-boletia-brand-management-organization-dark-background-angle-text-logo.png'/>
    </header>
 }
--- src/componets/Profile.jsx
+++ src/componets/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import profCss from './Profile.module.css'
 
 const Profile = () => {
-    return  <div className='content'>
+    return  <div className={profCss.content}>
     <div>
     <img src='https://media.istockphoto.com/id/1300296030/photo/tropical-beach-palm-trees-sea-wave-and-white-sand.jpg?s=612x612&w=0&k=20&c=6xdlJDxSp5FVUpQC7AOt3ooHYPd3-gTQNF6lmwX5krk='/>
     </div>
