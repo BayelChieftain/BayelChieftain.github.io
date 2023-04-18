314546acf7cab838d950f4cdfa0fce22bdc9abef
Bayel Chief <marvel.vovo@gmail.com>
2023-04-18 15:16:28 +0600

    add first pages

---
+++ src/componets/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return  <header className='header'>
+    <img src='https://w7.pngwing.com/pngs/187/532/png-transparent-logo-boletia-brand-management-organization-dark-background-angle-text-logo.png'/>
+   </header>
+}
+
+export default Header;
---
+++ src/componets/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return  <div className='content'>
+    <div>
+    <img src='https://media.istockphoto.com/id/1300296030/photo/tropical-beach-palm-trees-sea-wave-and-white-sand.jpg?s=612x612&w=0&k=20&c=6xdlJDxSp5FVUpQC7AOt3ooHYPd3-gTQNF6lmwX5krk='/>
+    </div>
+    <div>
+      ava + disrp
+    </div>
+    <div>
+      my post
+      <div>new post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 1</div>
+      </div>
+    </div>
+    
+    </div>
+}
+
+export default Profile;
