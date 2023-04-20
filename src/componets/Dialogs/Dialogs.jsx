1ff11577b0776d30503cafe029f91bd3c187983e
Bayel Chief <marvel.vovo@gmail.com>
2023-04-20 22:10:05 +0600

    setting setting component

--- src/componets/Dialogs/Dialogs.jsx
+++ src/componets/Dialogs/Dialogs.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-// import c from './Dialogs.module.css'
+import c from './Dialogs.module.css'
 
 const Dialog = (props) => {
     return (
-    <div>
-        gg1gggggg
+    <div className={c.dialogs}>
+        <div className={c.dialogsItems}>
+            <div className={c.dialog}>
+                dymych
+            </div>
+            <div className={c.dialog}>
+                dymych
+            </div>
+            <div className={c.dialog}>
+                dymych
+            </div>
+            <div className={c.dialog}>
+                dymych
+            </div>
+        </div>
+
+        <div className={c.messages}>
+            <div className={c.message}>Hi</div>
+            <div className={c.message}>How is</div>
+            <div className={c.message}>way samuraya</div>
+        </div>
     </div>
     )
 }
