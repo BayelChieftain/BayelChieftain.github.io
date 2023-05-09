131ef48ed64d1ccb7d9e1340595b2207621047a0
Bayel Chief <marvel.vovo@gmail.com>
2023-05-09 21:55:42 +0600

    rendering a container component

--- src/componets/Users/UsersContainer.jsx
+++ src/componets/Users/UsersContainer.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import axios, * as others from 'axios';
 import { connect } from "react-redux";
 import Users from "./Users";
 import { followCreator, setCurrentPageAC, setTotalCAC, setUsersAC, unFollowCreator } from "../redux/users-reducer";
+
+class UsersAPIcomponent extends React.Component {    
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPages}&count=${this.props.pageSize }`)
+            .then( response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalCounts(response.data.totalCount)
+            });
+    };
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize }`)
+            .then( response => {
+                this.props.setUsers(response.data.items)
+            });
+    };
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPages={this.props.currentPages}
+        onPageChanged={this.onPageChanged}
+        users={this.props.users}
+        unfollow={this.props.unfollow}
+        follow={this.props.follow}
+        
+        /> 
+    }
+}
 
 
 const mapStateToProps = (state) => {
@@ -36,4 +66,4 @@ const mapDispatchToProps = (dispatch) => {
  
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIcomponent);
