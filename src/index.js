19ece4ce3035721f877b83e60b289c15d92203d8
Bayel Chief <marvel.vovo@gmail.com>
2023-05-24 20:18:49 +0600

    deploy version

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './App';
 // импорт данных
 import store from './componets/redux/reduxStore';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
   root.render(
-    <BrowserRouter>
+    <HashRouter basename={process.evn.PUBLIC_URL}>
      <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
      </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
   );
 
 
