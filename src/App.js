42494bd6297a9a0ab187f6bc97b5443e59619bf5
Bayel Chief <marvel.vovo@gmail.com>
2023-05-27 22:14:51 +0600

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,92 @@
-import React, { Suspense } from 'react';
-import './App.css';
-import Navbar from './componets/Navbar/Navbar';
-import { Routes, Route } from "react-router-dom";
-import Setting from './componets/Setting/Setting';
-import News from './componets/News/News';
-import Music from './componets/Music/Music';
-import UsersContainer from './componets/Users/UsersContainer';
-import HeaderContainer from './componets/Header/HeaderContainer';
-import Login from './componets/Login/Login';
-import { connect } from 'react-redux';
-import {  initializeApp } from "./componets/redux/app-reducer";
-import Preloader from './componets/Preloader/Preloader';
+import React, { Suspense } from "react";
+import "./App.css";
+import Navbar from "./componets/Navbar/Navbar";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Setting from "./componets/Setting/Setting";
+import News from "./componets/News/News";
+import Music from "./componets/Music/Music";
+import UsersContainer from "./componets/Users/UsersContainer";
+import HeaderContainer from "./componets/Header/HeaderContainer";
+import Login from "./componets/Login/Login";
+import { connect } from "react-redux";
+import { initializeApp } from "./componets/redux/app-reducer";
+import Preloader from "./componets/Preloader/Preloader";
 
-const DialogContainer = React.lazy( () => import('./componets/Dialogs/DialogsContainer'));
-const ProfileContainer = React.lazy( () => import('./componets/Profile/ProfileContainer'));
+const DialogContainer = React.lazy(() =>
+  import("./componets/Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("./componets/Profile/ProfileContainer")
+);
 
+class App extends React.Component {
+  componentDidMount() {
+    this.props.initializeApp();
+  }
 
-class App extends React.Component  {
-  componentDidMount()  {
-    this.props.initializeApp ();
- }
+  render() {
+    if (!this.props.initialized) {
+      return <Preloader />;
+    }
+    return (
+      <div className="app-wrapper">
+        <HeaderContainer />
 
-  render () {
-    if (!this.props.initialized){
-    return <Preloader />}
-  return (  
-    <div className='app-wrapper'>
-      
-      
-      <HeaderContainer />
+        <Navbar />
 
-      <Navbar />
-      
-      <div className='app-wrapper-content'> 
-      <Routes>
-        <Route path='/dialog' element={
-          <Suspense>  <DialogContainer /> </Suspense>
-       
-       }  />
-        
-        
-        <Route path="/profile" element={<Suspense> <ProfileContainer /> </Suspense> }>
-        <Route path=":userId" element={ <Suspense> <ProfileContainer /> </Suspense> } />
-      </Route>
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route exact path="/" element={<Navigate to={"/profile"} />} />
+            <Route
+              path="/dialog"
+              element={
+                <Suspense>
+                  <DialogContainer />
+                </Suspense>
+              }
+            />
 
-            <Route path='/users' element={ <UsersContainer /> }/> 
+            <Route
+              path="/profile"
+              element={
+                <Suspense>
+                  <ProfileContainer />
+                </Suspense>
+              }
+            >
+              <Route
+                path=":userId"
+                element={
+                  <Suspense>
+                    <ProfileContainer />
+                  </Suspense>
+                }
+              />
+            </Route>
 
-            <Route path='/login' element={<Login />} />
+            <Route path="/users" element={<UsersContainer />} />
 
+            <Route path="/login" element={<Login />} />
 
-            <Route path='/news' element={<News />}/> 
-          <Route path='/music' element={<Music />}/> 
-        <Route path='/settings' element={<Setting />}/> 
-      </Routes>
-      
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Setting />} />
+            <Route
+              path="*"
+              element={
+                <div>
+                  <h1>404 NOT FOUND</h1>
+                </div>
+              }
+            />
+          </Routes>
+        </div>
       </div>
-      
-    </div>
-    
-  );
+    );
   }
 }
 const mapStateToProps = (state) => ({
-  initialized: state.app.initialized
-})
+  initialized: state.app.initialized,
+});
 
-export default connect(mapStateToProps, {initializeApp})(App);
-
+export default connect(mapStateToProps, { initializeApp })(App);
--- src/componets/Profile/ProfileItem/ProfileInfo.jsx
+++ src/componets/Profile/ProfileItem/ProfileInfo.jsx
@@ -1,80 +1,120 @@
 import React, { useState } from "react";
-import C from './ProfileInfo.module.css';
+import C from "./ProfileInfo.module.css";
 import Preloader from "../../Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
-import userPhoto from '../../../assts/img/interact.png';
+import userPhoto from "../../../assts/img/interact.png";
 import ProfileForm from "./ProfileForm";
 import { saveProfile } from "../../redux/profile-reducer";
 import { connect } from "react-redux";
 
+const ProfileInfo = (props) => {
+  let [editMode, setEditMode] = useState(false);
 
+  if (!props.profile) {
+    return <Preloader />;
+  }
 
-const  ProfileInfo = (props) => {
+  const onMainPhotoSelected = (e) => {
+    if (e.target.files.length) {
+      props.savePhoto(e.target.files[0]);
+    }
+  };
 
-    let [editMode, setEditMode] = useState(false);
-
-    if (!props.profile) {
-      return  <Preloader />
-    };
-
-    const onMainPhotoSelected = (e) => {
-        if (e.target.files.length) {
-            props.savePhoto(e.target.files[0])
-        }
-    };
-
-    return  <div>
-        <div className={C.discrpBlock}>
-            <img className={C.mainPhoto} src={props.profile.photos.large || userPhoto}/>
-            { props.isOwner && <div className={C.mainLoadPhoto}>
-            <h4>Загрузить фото профиля</h4>
-            <div> { props.isOwner && <input className={C.inputPhoto} type="file" onChange={ onMainPhotoSelected } /> }</div>
-            </div>}
-            { editMode ? <ProfileForm saveProfile={ props.saveProfile } profile={ props.profile }
-            setEditMode={ () => setEditMode(false)} />
-            : <ProfileData profile={ props.profile } setEditMode={ () => setEditMode(true)} isOwner={props.isOwner} />  }
-        
-
-            <ProfileStatusWithHooks updateStatus={props.updateStatus} status={props.status} />
-        </div>
-    </div>
-}
-
-const ProfileData = ({profile, isOwner, setEditMode}) => {
-   return <div>
-   { isOwner && <div> <button onClick={setEditMode}>EDIT</button></div>}
+  return (
     <div>
-        <b>Full name</b> {profile.fullName}
+      <div className={C.discrpBlock}>
+        <img
+          className={C.mainPhoto}
+          src={props.profile.photos.large || userPhoto}
+        />
+        {props.isOwner && (
+          <div className={C.mainLoadPhoto}>
+            <h4>Загрузить фото профиля</h4>
+            <div>
+              {" "}
+              {props.isOwner && (
+                <input
+                  className={C.inputPhoto}
+                  type="file"
+                  onChange={onMainPhotoSelected}
+                />
+              )}
+            </div>
+          </div>
+        )}
+        {editMode ? (
+          <ProfileForm
+            saveProfile={props.saveProfile}
+            profile={props.profile}
+            setEditMode={() => setEditMode(false)}
+          />
+        ) : (
+          <ProfileData
+            profile={props.profile}
+            setEditMode={() => setEditMode(true)}
+            isOwner={props.isOwner}
+          />
+        )}
+
+        <ProfileStatusWithHooks
+          updateStatus={props.updateStatus}
+          status={props.status}
+        />
+      </div>
     </div>
-   
-   <div>
-    <b> Looking for a job: </b> {profile.lookingForAJob ? 'yes' : 'no'}
-   </div>
+  );
+};
 
-   <div>
-    <b> Looking for a job Description: </b> {profile.lookingForAJobDescription}
-   </div>
-   
-   <div>
-   <b> About me </b>  {profile.aboutMe}
-   </div>
-
-   <div>
-   <b> Contacts </b>  {Object.keys(profile.contacts).map( key => {
-        return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
-   })}
-   </div>
-
-</div>
-}
-
-
-const Contact = ({contactTitle, contactValue}) => {
-    return (
-        <div className={C.contact}>
-            <b>{contactTitle}</b>: {contactValue ? contactValue : '---'}
+const ProfileData = ({ profile, isOwner, setEditMode }) => {
+  return (
+    <div>
+      {isOwner && (
+        <div>
+          <button onClick={setEditMode}>EDIT</button>
         </div>
-    )
-}
+      )}
+      <div>
+        <b>Full name</b> {profile.fullName}
+      </div>
 
-export default connect(null, {saveProfile})(ProfileInfo);
+      <div>
+        <b> Looking for a job: </b> {profile.lookingForAJob ? "yes" : "no"}
+      </div>
+
+      <div>
+        <b> Looking for a job Description: </b>
+        {profile.lookingForAJobDescription}
+      </div>
+
+      <div>
+        <b> About me </b> {profile.aboutMe}
+      </div>
+
+      <div>
+        <b> Contacts </b>
+        {Object.keys(profile.contacts).map((key) => {
+          return (
+            <Contact
+              key={key}
+              contactTitle={key}
+              contactValue={profile.contacts[key]}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Contact = ({ contactTitle, contactValue }) => {
+  return (
+    <div className={C.contact}>
+      <b>{contactTitle}</b>:
+      <a className={C.contactLink} href={contactValue ? contactValue : "---"}>
+        {contactValue ? contactValue : "---"}
+      </a>
+    </div>
+  );
+};
+
+export default connect(null, { saveProfile })(ProfileInfo);
