b5002f4969e98afda1499b1d13bfcb1bc6e35fc8
Bayel Chief <marvel.vovo@gmail.com>
2023-05-31 21:55:19 +0600

    added spotify widget for listening to music

--- src/componets/Music/Music.jsx
+++ src/componets/Music/Music.jsx
@@ -2,9 +2,18 @@ import React from "react";
 // import c from './Music.module.css'
 
 const Music = (props) => {
+    const playlistId = '6VugrLq7qoCFPV78p7LirB';
     return (
     <div>
-        MUSIC
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/6VugrLq7qoCFPV78p7LirB?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '616px' }}
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
     </div>
     )
 }
